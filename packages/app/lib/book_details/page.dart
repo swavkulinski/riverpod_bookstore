8523a77be052ff6df604e36c6f8bfce93abe8c66
Robotoaster <swav.kulinski@gmail.com>
2021-11-10 10:32:36 +0000

    extracted book details content

--- packages/app/lib/book_details/page.dart
+++ packages/app/lib/book_details/page.dart
@@ -8,18 +8,25 @@ class BookDetailsPage extends StatelessWidget {
   const BookDetailsPage({Key? key, required this.book}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => BookstoreScaffold(
-        titleLabel: book.title,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            _EntryWidget(title: 'Title', value: book.title),
-            _EntryWidget(title: 'Author', value: book.author),
-            _EntryWidget(title: 'Category', value: book.category.name),
-            _EntryWidget(title: 'Synopsis', value: book.synopsis)
-          ],
-        ),
+  Widget build(BuildContext context) =>
+      BookstoreScaffold(titleLabel: book.title, child: _BookDetailsPage(book: book));
+}
+
+class _BookDetailsPage extends StatelessWidget {
+  final Book book;
+
+  const _BookDetailsPage({Key? key, required this.book}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          _EntryWidget(title: 'Title', value: book.title),
+          _EntryWidget(title: 'Author', value: book.author),
+          _EntryWidget(title: 'Category', value: book.category.name),
+          _EntryWidget(title: 'Synopsis', value: book.synopsis)
+        ],
       );
 }
 
@@ -31,14 +38,14 @@ class _EntryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Row(
-    mainAxisAlignment: MainAxisAlignment.start,
-    crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ConstrainedBox(
             constraints: const BoxConstraints.expand(width: 80, height: 20),
             child: Text(title),
           ),
-          Expanded(child:Text(value)),
+          Expanded(child: Text(value)),
         ],
       );
 }
